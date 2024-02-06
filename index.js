e187c81ad9d8d664da7c79385950bb80070640ac
iNitishSingh <nitish8097@gmail.com>
2024-02-06 23:39:36 +0530

    till gc

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const { error } = require('console');
-const config = require('./5190009379.json');
-const fs=require('fs');
-const { finished } = require('stream');
-const  xlsx= require("xlsx");
-let workbook=xlsx.readFile("./EC_CLAUSE_MASTER.xlsx");
-
+import policy_json from './5190009379.json' assert { type: "json" };
+import node_fetch from 'node-fetch'
+import fs from 'fs';
+import  xlsx from "xlsx";
+import { json } from 'stream/consumers';
+const config = policy_json.data[0];
+let workbook=xlsx.readFile("./EC_CLAUSE_MASTER.xlsx");//reading clauses masster
 let worksheet=workbook.Sheets[workbook.SheetNames[0]]
 let range=xlsx.utils.sheet_to_json(worksheet);
 
-//console.log(range[0].CLAUSE_DESCRIPTION);
+config.proposal.data.location_code="90200";
+if (config.proposal.data.channel_mapper_name ==  "Agency"){
 
-//console.log(workbook);
-//console.log(config)
 
-//console.log(config.proposal.data.work_trade_catg_1)
+    config.policy.broker_code="2123415667";
+}
+else if(config.proposal.data.channel_mapper_name ==  "Broker"){
+    config.policy.broker_code="9890248138";   
+}
+else if(config.proposal.data.channel_mapper_name ==  "Banca"){
+    config.policy.broker_code="9977137677"; 
+}
 
-employeeDetails=[];
+else{
+    console.log(config.policy.broker_code+"-"+ config.proposal.data.broker_name+"Not present")
+}
+if(config.proposal.data.employeeDetails == undefined ){
+
+    employeeDetails=[];
 clausesMaster=[];
 
  var count=0;
 
  let tradeObj=[]
 
+ 
  //console.log(config.proposal.data[`trade_catg_${1}`]);
 
 // for(let i=1;i<10;i++){
@@ -42,8 +54,9 @@ clausesMaster=[];
 // console.log(tradeObj[0].trade_code_name)
 // console.log(count)
 
+//console.log(config.quote.data.worker_detail_count)
 
-for(let i=1;i<Number(config.proposal.data.worker_detail_count);i++){
+for(let i=1;i<=Number(config.quote.data.worker_detail_count);i++){
 
     // let k;
     // for(j=0;j<count;j++){
@@ -54,19 +67,16 @@ for(let i=1;i<Number(config.proposal.data.worker_detail_count);i++){
     // }
     // console.log("k="+k)
     employeeDetails.push({
-        tradeCategory:config.quote.data[`work_trade_catg_${1}`],
-        monthlyWages:config.proposal.data[`monthly_wages_${i}`],
-        numOfWorkers:config.proposal.data[`no_of_worker_${i}`],
-        typeOfWorkers:config.proposal.data[`work_catg_${i}`],
-        totalWages:config.proposal.data[`total_wages_${i}`],
-        ecRate:config.proposal.data[`worker_rate_${i}`],
-        grouping:config.proposal.data[`work_trade_catg_group_${i}`]
+        tradeCategory:config.quote.data[`work_trade_catg_${i}`],
+        monthlyWages:config.quote.data[`monthly_wages_${i}`],
+        numOfWorkers:config.quote.data[`no_of_worker_${i}`],
+        typeOfWorkers:config.quote.data[`work_catg_${i}`],
+        totalWages:config.quote.data[`total_wages_${i}`],
+        ecRate:config.quote.data[`worker_rate_${i}`],
+        grouping:config.quote.data[`work_trade_catg_group_${i}`]
     })
 
 }
-
-console.log(config.quote.data.trade_category_details[0].length_increase)
-
 
 let premiumDetails=[]
 
@@ -88,7 +98,6 @@ for(let i=1;i<15;i++){
         for(let j=0;j<range.length;j++){
             if(config.proposal.data[`clause_${i}`]==range[j].CLAUSE_DESCRIPTION){
                 k=range[j].CLAUSE_CODE;
-                //console.log(k)
             }
         }
         clausesMaster.push({
@@ -99,20 +108,72 @@ for(let i=1;i<15;i++){
 }
 }
 
-// console.log(employeeDetails)
- console.log(premiumDetails)
-// console.log(clausesMaster)
-//const nks =JSON.parse(JSON.stringify(config));
 config.proposal.data['employeeDetails']= employeeDetails;
 config.proposal.data['premiumDetails']=premiumDetails;
 config.proposal.data['clausesMaster']=clausesMaster
+}
 
-//console.log(config.proposal.data.clausesMaster)
+else{
+    console.log("required")
+}
+
 fs.writeFile('policy.json',JSON.stringify(config),(error)=>{
     if(error){
         console.log(error)
         return;
     }
+    else{
+        console.log("done")
+    }
 })
+
+const auth={
+    email: "uw",
+    mpwd: "098f6bcd4621d373cade4e832627b4f6"
+}
+const header = new Headers({
+    'Content-Type': 'application/json'
+});
+
+let authresp;
+
+const response = await node_fetch('https://uatis2.cloware.in/api/v1/auth',{
+    method:"POST",
+    headers:header,
+    body:JSON.stringify(auth)
+});
+const resp = await response.json();
+
+const token = resp.data.token;
+
+console.log(token)
+ let twig =fs.readFileSync('./Ecproposal.twig','utf8',function(error,data){
+    console.log(error)
+ })
+
+ header.append("Authorization",`Bearer ${token}`);
+
+ const ec_request_obj={
+
+    json:JSON.stringify(config),
+    twig:twig
+
+ }
+
+ const ec_request=await node_fetch('https://uatis2.cloware.in/api/v1/twigtest',{
+    method:"POST",
+    headers:header,
+    body:JSON.stringify(ec_request_obj)
+})
+let ec_re =await ec_request.json();
+
+const Ecproposal=await node_fetch('https://connectbeta.tataaiginsurance.in/integration/PACERestService/webServiceEC',{
+    method:"POST",
+    headers:header,
+    body:ec_re.data.data
+})
+ const Ecproposal_resp =await Ecproposal.json();
+
+ console.log(Ecproposal_resp.errorLog.errorLog[0])
 
 
